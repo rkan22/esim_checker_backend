371842b59bd1cce52c3e80d840998b93d789b141
Aizan Ahmed <malikaizan198@gmail.com>
2025-11-17 16:43:30 +0500

    Added files for render

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
+
